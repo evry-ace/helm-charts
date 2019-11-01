9be5a12c744b222740746fe8876d665e02cc3839
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-11-01 22:54:34 +0100

    Add initial workflow

--- test.sh
+++ test.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
+
 set -u
 set -e
 set -o pipefail
 
-helm lint *
+for chart in */Chart.yaml; do
+  chart=$(dirname $chart)
 
-for dir in test/*/; do
-  chart=$(basename ${dir})
-
-  for file in ${dir}*; do
-    echo "${chart} - $(basename ${file})"
-    helm template ${chart} -f ${file} --name=test-dev | kubeval --strict
-  done
+  helm lint $chart
 done
