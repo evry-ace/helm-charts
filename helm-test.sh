8d5b455d24fb76d3c8bdbaeb7a9bc2d7f4463f32
Knut Ivar Alvestad <knut.alvestad@tietoevry.com>
2024-06-21 12:56:25 +0200

    dont remake temp

--- helm-test.sh
+++ helm-test.sh
@@ -14,7 +14,7 @@ for chart in */Chart.yaml; do
   helm lint $chart
   [ ! -z "$(echo $chart | egrep 'dotnet|golang|java|nodejs|web')" ] && {
     [ -d data ] && rm -rf data
-    mkdir data temp
+    # mkdir data temp
     helm template $chart --output-dir temp --set istio.enabled=1 --set csi.enabled=1
     for f in temp/$chart/templates/*.yaml; do dst="data/$(basename $f | sed 's/yaml/json/g')"; yq eval $f -o json > $dst; done
     conftest test data/*
